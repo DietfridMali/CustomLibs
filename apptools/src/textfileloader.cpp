730f6f5b53c9ac4de262e0f88d215c1a2fbc3ee5
DietfridMali <karx11erx@googlemail.com>
2025-09-03 16:58:57 +0200

    removed some debug code

--- apptools/src/textfileloader.cpp
+++ apptools/src/textfileloader.cpp
@@ -7,16 +7,12 @@
 #include <string>
 #include <algorithm>
 
-extern void DebugCheck(void);
-
 // =================================================================================================
 
 TableDimensions TextFileLoader::ReadLines(const char * fileName, List<String>& textLines, tLineFilter filter) {
-    DebugCheck();
     std::ifstream stream(fileName);
     if (not stream.is_open())
         return TableDimensions(0,0);
-    DebugCheck();
     return ReadStream(stream, textLines, filter);
 }
 
@@ -31,16 +27,12 @@ TableDimensions TextFileLoader::ReadStream(std::istream& stream, List<String>& t
     std::string line;
     int rows = 0;
     int cols = 0;
-    DebugCheck();
     while (std::getline(stream, line)) {
-        DebugCheck();
         String s(line);
-        DebugCheck();
         if (filter(s)) {
             rows++;
             cols = std::max(cols, int(line.length()));
             textLines.Append(std::move(s));
-            DebugCheck();
         }
     }
     return TableDimensions(cols, rows);
