5f1432164184933e3789aa4ca6647f67e28bd77d
DietfridMali <karx11erx@googlemail.com>
2025-09-09 01:28:29 +0200

    updates to SpeedRayEffect (shader not yet working)

--- basetools/src/glm_vector.cpp
+++ basetools/src/glm_vector.cpp
@@ -9,11 +9,11 @@ const Vector2f Vector2f::ONE = Vector2f({ 1.0f, 1.0f });
 
 const Vector3f Vector3f::ZERO = Vector3f{ 0.0f, 0.0f, 0.0f };
 const Vector3f Vector3f::NONE = Vector3f{ NAN, NAN, NAN };
-const Vector3f Vector2f::HALF = Vector2f({ 0.5f, 0.5f, 0.5f });
+const Vector3f Vector3f::HALF = Vector2f({ 0.5f, 0.5f, 0.5f });
 const Vector3f Vector3f::ONE = Vector3f{ 1.0f, 1.0f, 1.0f };
 
 const Vector4f Vector4f::ZERO = Vector4f{ 0.0f, 0.0f, 0.0f, 0.0f };
-const Vector3f Vector2f::HALF = Vector2f({ 0.5f, 0.5f, 0.5f, 0.5f });
+const Vector4f Vector4f::HALF = Vector2f({ 0.5f, 0.5f, 0.5f, 0.5f });
 const Vector4f Vector4f::ONE = Vector4f{ 1.0f, 1.0f, 1.0f, 1.0f };
 const Vector4f Vector4f::NONE = Vector4f{ NAN, NAN, NAN, NAN };
 
