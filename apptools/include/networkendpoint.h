b4fe73f0cc99a2fd26e7614b27f4df0f96e1278d
DietfridMali <karx11erx@googlemail.com>
2025-09-30 19:19:49 +0200

    added subnet calculation to NetworkEndpoint

--- apptools/include/networkendpoint.h
+++ apptools/include/networkendpoint.h
@@ -88,6 +88,10 @@ public:
         return *this;
     }
 
+    inline uint32_t SubNet(void) noexcept {
+        return m_socketAddress.host & SDL_SwapBE32(0xFFFFFF00u);
+    }
+
 private:
     void UpdateFromSocketAddress(void);
 
