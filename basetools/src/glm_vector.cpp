77bcdd2cb0cad2525783e0ac3d15f5d434338ab1
DietfridMali <karx11erx@googlemail.com>
2025-10-13 16:17:26 +0200

    code formatting

--- basetools/src/glm_vector.cpp
+++ basetools/src/glm_vector.cpp
@@ -13,8 +13,8 @@ const Vector3f Vector3f::HALF = Vector3f({ 0.5f, 0.5f, 0.5f });
 const Vector3f Vector3f::ONE  = Vector3f({ 1.0f, 1.0f, 1.0f });
 
 const Vector4f Vector4f::ZERO = Vector4f({ 0.0f, 0.0f, 0.0f, 0.0f });
+const Vector4f Vector4f::NONE = Vector4f({ NAN, NAN, NAN, NAN });
 const Vector4f Vector4f::HALF = Vector4f({ 0.5f, 0.5f, 0.5f, 0.5f });
 const Vector4f Vector4f::ONE  = Vector4f({ 1.0f, 1.0f, 1.0f, 1.0f });
-const Vector4f Vector4f::NONE = Vector4f({ NAN, NAN, NAN, NAN });
 
 #endif
