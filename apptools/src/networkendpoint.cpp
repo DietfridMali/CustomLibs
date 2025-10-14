3b10fac620ce44658187ed732196d68c24a09dd3
DietfridMali <karx11erx@googlemail.com>
2025-10-14 18:06:44 +0200

    made format statements for IP address misinterpreation proof for g++

--- apptools/src/networkendpoint.cpp
+++ apptools/src/networkendpoint.cpp
@@ -7,7 +7,7 @@ void NetworkEndpoint::UpdateFromSocketAddress(void) {
     uint32_t host = SDL_SwapBE32(m_socketAddress.host);
     m_port = SDL_SwapBE16(m_socketAddress.port);
     uint8_t* p = reinterpret_cast<uint8_t*>(&host);
-    m_ipAddress.Format("{}.{}.{}.{}", p[3], p[2], p[1], p[0]);
+    m_ipAddress.Format("{:d}.{:d}.{:d}.{:d}", (unsigned) ((host >> 24) & 0xFF), (unsigned)((host >> 16) & 0xFF), (unsigned)((host >> 8) & 0xFF), (unsigned)(host & 0xFF));
 }
 
 
