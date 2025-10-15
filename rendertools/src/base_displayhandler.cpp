feaef52df75566209dcf23385b9358dce2563eca
DietfridMali <karx11erx@googlemail.com>
2025-10-15 20:47:10 +0200

    updated makefiles made BaseDisplayHandler::SetupDisplay safer

--- rendertools/src/base_displayhandler.cpp
+++ rendertools/src/base_displayhandler.cpp
@@ -49,30 +49,49 @@ void BaseDisplayHandler::SetupDisplay(String windowTitle) {
         if ((m_width != m_maxWidth) or (m_height != m_maxHeight))
             screenType |= SDL_WINDOW_BORDERLESS;
         else
-            screenType |= SDL_WINDOW_FULLSCREEN;
+            screenType |= SDL_WINDOW_FULLSCREEN_DESKTOP;
         m_fullscreen = true;
     }
     m_aspectRatio = float(m_width) / float(m_height);
 #if 1
-    m_window = SDL_CreateWindow(windowTitle, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, m_width, m_height, screenType);
+    SDL_GL_ResetAttributes();
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+
+    try {
+        m_window = SDL_CreateWindow(windowTitle, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, m_width, m_height, screenType);
+    }
+    catch (...) {
+        m_window = nullptr;
+    }
     if (not m_window) {
-        fprintf(stderr, "Couldn't set screen mode (%d x %d)\n", m_width, m_height);
+        fprintf(stderr, "Couldn't set screen mode (%d x %d) (error '%s')\n", m_width, m_height, SDL_GetError());
         exit(1);
     }
-    m_context = SDL_GL_CreateContext(m_window);
+    try {
+        m_context = SDL_GL_CreateContext(m_window);
+    }
+    catch (...) {
+        m_context = nullptr;
+    }
     if (not m_context) {
-        char msg[200];
-        sprintf (msg, "Error '%s' when trying to create OpenGL context\n", SDL_GetError());
-        throw std::runtime_error(msg);
+        fprintf(stderr, "Could not get OpenGL context (error '%s')\n", SDL_GetError());
+        exit(1);
     }
     SDL_GL_SetSwapInterval(m_vSync ? 1 : 0);
 #endif
 }
 
-
 void BaseDisplayHandler::Update(void) {
     SDL_GL_SwapWindow(m_window);
 }
+
 
 BaseDisplayHandler* baseDisplayHandlerInstance = nullptr;
 
